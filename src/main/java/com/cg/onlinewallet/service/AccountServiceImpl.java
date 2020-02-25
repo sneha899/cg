684e36ccf5700e40d14260ae63ddae849b602546
sneha899 <52658662+sneha899@users.noreply.github.com>
2020-02-25 01:31:48 +0100

    implemented the methods

--- src/main/java/com/cg/onlinewallet/service/AccountServiceImpl.java
+++ src/main/java/com/cg/onlinewallet/service/AccountServiceImpl.java
@@ -1,5 +1,47 @@
 package com.cg.onlinewallet.service;
 
-public class AccountServiceImpl {
+import com.cg.onlinewallet.bean.WalletAccount;
+import com.cg.onlinewallet.dao.AccountDao;
+import com.cg.onlinewallet.dao.AccountDaoMapImpl;
+import com.cg.onlinewallet.exception.AccountException;
+
+public class AccountServiceImpl implements AccountService {
+	
+	AccountDao accountDao = new AccountDaoMapImpl();
+	 
+    WalletAccount walletAccount = new WalletAccount();
+	
+    @Override
+	public boolean createWalletAccount(WalletAccount account) throws AccountException
+	{
+    	return accountDao.createWalletAccount(account);
+	}
+
+    @Override
+	public boolean depositAccount(WalletAccount account, double amount) throws AccountException{
+		boolean isDeposited=false;
+    	if(amount != 0 || amount > 0){
+			accountDao.depositAccount(account, amount);
+			isDeposited =true;
+    	}
+			else
+				throw new AccountException("deposit amount can not be zero or a negative value");
+		return isDeposited;
+			
+			
+	}
+    
+    @Override
+	public double showBalance(WalletAccount account) throws AccountException{
+		
+			return accountDao.showBalance(account);
+	}
+	
+	@Override
+	public boolean transferAmount(WalletAccount fromAccount, WalletAccount toAccount, double amount) throws AccountException{
+		return accountDao.transferAmount(fromAccount, toAccount, amount);
+		
+	}
+
 
 }
