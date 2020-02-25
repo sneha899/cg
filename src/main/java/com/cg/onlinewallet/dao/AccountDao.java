8474606bec19dc7d22effdb23b3228cc809a07e3
sneha899 <52658662+sneha899@users.noreply.github.com>
2020-02-25 01:27:18 +0100

    updated the AccountDao

--- src/main/java/com/cg/onlinewallet/dao/AccountDao.java
+++ src/main/java/com/cg/onlinewallet/dao/AccountDao.java
@@ -4,11 +4,10 @@ import com.cg.onlinewallet.bean.WalletAccount;
 import com.cg.onlinewallet.exception.AccountException;
 
 public interface AccountDao {
-	public int createWalletAccount(WalletAccount account) throws AccountException;
+	public boolean createWalletAccount(WalletAccount account) throws AccountException;
 
-	public int depositAccount(int accoundId, double amount) throws AccountException;
-	public double showBalance(int accountId) throws AccountException; 
-	public void withdrawAmount(int accountId, double amount);
-	public void transferAmount(int fromAccountId, int toAccountId2, double amount) throws AccountException;
-
+	public boolean depositAccount(WalletAccount accound, double amount) throws AccountException;
+	public double showBalance(WalletAccount account) throws AccountException; 
+	public boolean withdrawAmount(WalletAccount account, double amount) throws AccountException;
+	public boolean transferAmount(WalletAccount fromAccount, WalletAccount toAccount, double amount) throws AccountException;
 }
