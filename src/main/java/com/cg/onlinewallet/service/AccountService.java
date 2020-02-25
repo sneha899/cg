a426a038d4c2d6a5843457cb8176c4304c663e67
sneha899 <52658662+sneha899@users.noreply.github.com>
2020-02-25 01:31:06 +0100

    Updated the methods

--- src/main/java/com/cg/onlinewallet/service/AccountService.java
+++ src/main/java/com/cg/onlinewallet/service/AccountService.java
@@ -1,62 +1,20 @@
 package com.cg.onlinewallet.service;
 
-import java.util.UUID;
 
 import com.cg.onlinewallet.dao.AccountDaoMapImpl;
 import com.cg.onlinewallet.bean.WalletAccount;
-import com.cg.onlinewallet.bean.WalletTransaction;
 import com.cg.onlinewallet.exception.AccountException;
 
-public class AccountService {
+public interface AccountService {
 
-	
-	
-	/*UUID uuid = UUID.randomUUID();
-    String randomUUIDString = uuid.toString();
-    WalletTransaction walletTransaction = new WalletTransaction();*/
-    
     WalletAccount walletAccount = new WalletAccount();
 	
 	AccountDaoMapImpl accountDao= new AccountDaoMapImpl();
-	public void validateAndCreateWalletAccount(WalletAccount account) throws AccountException
-	{
-		accountDao.createWalletAccount(account);
-	}
-
-	public void validateAndDepositAccount(int accountId, double amount) throws AccountException{
-		if(amount >0){
-			WalletAccount account = accountDao.getAccounts().get(accountId);
-			if(account != null){
-				account.setBalance(account.getBalance()+amount);
-			}
-			else 
-				throw new AccountException("Account not found with given account number"); 
-			
-		} else {
-			throw new AccountException("deposit amount can not be zero or a negative value"); 
-		}
-	}
-	public double validateAndShowBalance(int accountId) throws AccountException{
-		WalletAccount account = accountDao.getAccounts().get(accountId);
-		if(account != null)
-			return account.getBalance();
-		else 
-			throw new AccountException("Account not found with given account number"); 
-	}
-	public void validateAndTransferAmount(int fromAccountId, int toAccountId, double amount) throws AccountException{
-		WalletAccount fromAcctWallet = accountDao.getAccounts().get(fromAccountId);
-		if( fromAcctWallet == null)
-		    throw new AccountException("Invalid sender account or account number not found"); 
-		WalletAccount toAcctWallet = accountDao.getAccounts().get(toAccountId);
-		if(toAcctWallet == null)
-			throw new AccountException("Invalid receiver account or account number not found"); 
-		
-		if(fromAcctWallet.getBalance() >= amount ){
-			accountDao.transferAmount(fromAccountId, toAccountId, amount);
-		}
-		else
-			throw new AccountException("Insuffiecient balance"); 
-		
-	}
+	public boolean createWalletAccount(WalletAccount account) throws AccountException;
 	
+	public boolean depositAccount(WalletAccount account, double amount) throws AccountException;
+	public boolean transferAmount(WalletAccount fromAccount, WalletAccount toAccount, double amount) throws AccountException;
+
+	public double showBalance(WalletAccount account) throws AccountException;
+		
 }
