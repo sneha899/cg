aabdc24eba3a550e5d043b212e5c88033b87baa2
sneha899 <52658662+sneha899@users.noreply.github.com>
2020-02-25 01:29:18 +0100

    Added exception messages

---
+++ src/main/java/com/cg/onlinewallet/exception/AccountExceptionDescription.java
@@ -0,0 +1,9 @@
+package com.cg.onlinewallet.exception;
+
+public class AccountExceptionDescription  {
+
+	public static final String invalidAccount = "Account number not found or Invalid account";
+	public static final String inSufficientFundsException = "Insufficient balance to withdraw amount";
+	public static final String transferAmountException = "Requested amount can not be transfer due to low balance";
+	
+}
